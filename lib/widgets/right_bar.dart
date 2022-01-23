1661990f095e8928e1f282b4d2d6e8a7ba177634
VishnuKC1105 <91648418+VishnuKC1105@users.noreply.github.com>
2022-01-23 08:30:47 +0530

    first commit

---
+++ lib/widgets/right_bar.dart
@@ -0,0 +1,27 @@
+import 'package:weight_calc/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class RightBar extends StatelessWidget {
+  const RightBar({Key? key, required this.barWidth}) : super(key: key);
+
+  final double barWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              color: accentHexColor),
+        )
+      ],
+    );
+  }
+}
